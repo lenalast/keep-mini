d66f0636a229fcde21c20137fe55de7092deb949
lenalast <lenalast@gmail.com>
2017-09-21 10:44:39 +0200

    Saves the checked todos to the API, so when you reender the page, it stays done

--- client/componentes/TodoList.js
+++ client/componentes/TodoList.js
@@ -1,6 +1,6 @@
 import React, {Component} from 'react';
 import CreateTodos from './CreateTodos';
-import get from 'axios';
+import axios from 'axios';
 import {Route, Link} from 'react-router-dom';
 
 class TodoList extends Component {
@@ -12,7 +12,7 @@ class TodoList extends Component {
   }
 
   componentDidMount() {
-    get('http://localhost:4000/todos')
+    axios.get('http://localhost:4000/todos')
       .then(res => this.setState({todos: res.data}))
       .catch(err => console.error(err))
   }
@@ -25,28 +25,28 @@ class TodoList extends Component {
         <CreateTodos createList={this.createList.bind(this)}/>
         <div className="todo-container">
           <Route exact path="/lists" render={() =>
-              <div className="tasks-list">
-                  <h2>Unchecked todos</h2>
-                  {
-                    todos.filter(todo => !todo.isDone)
-                      .map((todo, i) =>
-                        <div key={i} className="list" onClick={() => this.toggleTask(todo)}>{todo.desc}</div>
-                      )
-                  }
-                  <Link className="list-link" to="/lists/done">See All Done</Link>
+            <div className="tasks-list">
+              <h2>Unchecked todos</h2>
+              {
+                todos.filter(todo => !todo.isDone)
+                  .map((todo, i) =>
+                    <div key={i} className="list" onClick={() => this.toggleTask(todo)}>{todo.desc}</div>
+                  )
+              }
+              <Link className="list-link" to="/lists/done">See All Done</Link>
             </div>
           }/>
           <Route exact path="/lists/done" render={() =>
             <div className="tasks-list">
               <div>
-              <h2>Checked todos</h2>
-              {
-                todos.filter(todo => todo.isDone)
-                  .map((todo, i) =>
-                    <div key={i} className="checked list" onClick={() => this.toggleTask(todo)}>{todo.desc}</div>
-                  )
-              }
-              <Link className="list-link" to="/lists">See todos</Link>
+                <h2>Checked todos</h2>
+                {
+                  todos.filter(todo => todo.isDone)
+                    .map((todo, i) =>
+                      <div key={i} className="checked list" onClick={() => this.toggleTask(todo)}>{todo.desc}</div>
+                    )
+                }
+                <Link className="list-link" to="/lists">See todos</Link>
               </div>
             </div>
           }/>
@@ -59,12 +59,28 @@ class TodoList extends Component {
     const {todos} = this.state;
     todo.isDone = !todo.isDone;
     this.setState({todos})
+    this.saveTodoDone(todo)
   }
 
   createList(task) {
     this.state.todos.push(task);
     this.setState({todos: this.state.todos});
     console.log("the list:", this.state.todos)
+  }
+
+  saveTodoDone(todo) {
+    console.log(todo)
+    const params = {
+      desc: todo.desc,
+      isDone: todo.isDone,
+      id: todo.id
+    }
+
+    const id = todo.id
+
+    axios.patch('http://localhost:4000/todos/' + id, params)
+      .then(res => console.log(res.data))
+      .catch(err => console.error(err))
   }
 }
 
